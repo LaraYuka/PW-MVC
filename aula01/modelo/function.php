02888a0ae407357b13e8fdc39793e5dad1702e6d
LaraYuka <larayuka.sak@gmail.com>
2022-02-16 15:02:35 -0300

    teste-01

---
+++ aula01/modelo/function.php
@@ -0,0 +1,34 @@
+<?php
+
+    $NOME = $_REQUEST['NOME'];
+    $ANIMAL = $_REQUEST['ANIMAL'];
+
+    if(empty($NOME)){
+        $dados = array(
+            "tipo" => 'error',
+            "mensagem" => 'Existe(m) campo(s) a ser(em) preenhido(s).'
+        );
+    }else{
+
+        switch($ANIMAL){
+            case '1' : $dados = array(
+                "tipo" => 'cachorro',
+                "mensagem" => 'Seja bem vindo, '.$NOME.', sabemos que seu animal de preferencia é o cachorro'
+            );
+            break;
+
+            case '2' : $dados = array(
+                "tipo" => 'gato',
+                "mensagem" => 'Seja bem vindo, '.$NOME.', sabemos que seu animal de preferencia é o gato'
+            );
+            break;
+
+            case '3' : $dados = array(
+                "tipo" => 'peixe',
+                "mensagem" => 'Seja bem vindo, '.$NOME.', sabemos que seu animal de preferencia é o peixe'
+            );
+            break;
+        }
+
+        echo json_encode($dados);
+    }
