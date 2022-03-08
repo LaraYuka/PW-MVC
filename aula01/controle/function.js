a373abf35ac17dfe185d1663b5b2d3068835ea9b
larayuka <larayuka.sak@gmail.com>
2022-03-08 12:23:54 -0300

    aula 8-3-2022

--- aula01/controle/function.js
+++ aula01/controle/function.js
@@ -9,7 +9,36 @@ $(document).ready(function() {
         //Coletar o que foi escrito e selecionado em nosso formulário
         let dados = $('#form').serialize()
 
-        console.log(dados)
-    })
+        $('#retorno').empty()
 
+        //console.log(dados)
+
+        //ajax faz o retorno e tranforma em .php
+        //copiar correto se não dá erro
+        $.ajax({
+            type: 'POST',
+            dataType: 'JSON',
+            assync: true,
+            data: dados,
+            url: '../modelo/retorno.php',
+            success: function(dados) {
+                $('#retorno').append(`
+                <div class="col-12 col-sm-8 col-md-6">
+
+                <div class="alert-primary">
+                    <h1 class="text-center text-dark">
+                        ${dados.mensagem}
+                    </h1>
+                </div>
+    
+                <img src="../../img/${dados.tipo}" class="img-fluid">
+    
+            </div>
+                `)
+
+            }
+
+        })
+
+    })
 })
